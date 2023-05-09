8ec843b641ac67a3097d6ea23631705beb7b745c
X_x_X <X_x_X@DESKTOP-H7KETNC>
2023-05-09 16:32:39 +0300

    Добавьте файлы проекта.

---
+++ FDN.SQL.1/Core/DbModelContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDN.SQL._1.Core
+{
+    public static class DbModelContext
+    {
+        public static Task1Entities DB { get; set; } 
+    }
+}
---
+++ FDN.SQL.1/View/InfoWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FDN.SQL._1.View
+{
+    public partial class InfoWindow : Window
+    {
+        private UserInfoEntities _db = new UserInfoEntities();
+        public InfoWindow()
+        {
+            InitializeComponent();
+
+            DGInfo.ItemsSource = _db.Users.ToList();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            Close();
+        }
+    }
+}
+
+
